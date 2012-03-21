21f5a406f5b8ab6fcb7042fe67e682c3ab534155
Wess Cope <wcope@me.com>
2012-03-21 09:06:04 -0400

    Initial Commit

---
+++ FLKit/FLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLAppDelegate.h
+//  FLKit
+//
+//  Created by Wess Cope on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FLKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FLKit
+//
+//  Created by Wess Cope on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FLAppDelegate class]));
+    }
+}
